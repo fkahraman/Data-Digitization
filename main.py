d64afbe28025fe1b122da1271c68b1157c69c7c6
fatih kahraman <fatih.kahraman@32bit.com.tr>
2021-10-05 15:06:00 +0300

    Usage Types Added

--- main.py
+++ main.py
@@ -6,10 +6,21 @@
         Mail   : fatih.khrmn@hotmail.com
 """
 
+import pandas as pd
 from convertCSV import Converter
 
 if __name__ == '__main__':
 
-    converter = Converter('weather.csv')
-    converter.dropColumn("Day")
+    """
+        Usage Type 1
+    """
+    converter = Converter(path='weather.csv')
+    converter.dropColumn("Day") # Optional
+    converter.recordDf()
+
+    """
+        Usage Type 2
+    """
+    df = pd.read_csv('weather.csv')
+    converter = Converter(df=df)
     converter.recordDf()
